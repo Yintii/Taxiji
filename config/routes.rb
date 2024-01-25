3eb8a7a4af22daeaf9019247ae4396a68a06daf2
yintii <keleheart1@gmail.com>
2024-01-25 14:28:50 -0800

    Made home page and the route to go to help as well as implmented basic devise login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#home'
-  get 'static_pages/help'
+  #get the help page and set it's path to /help
+  get '/help', to: 'static_pages#help', as: 'help'
+  
+  #
+  #
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #
 
   # Defines the root path route ("/")
   # root "articles#index"
