67541d91776eb0e915bd4caee8d9982c83652c21
yintii <keleheart1@gmail.com>
2024-02-02 10:38:20 -0800

    working through cors issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception, unless: -> { request.headers['X-Requested-With'] == 'XMLHttpRequest' }
   before_action :authenticate_user!, except: [:home, :help]
 end
