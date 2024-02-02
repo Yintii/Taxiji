d373bc8f698fdf99118f5b4b599a9f42e3cbe16c
yintii <keleheart1@gmail.com>
2024-02-01 19:20:48 -0800

    route was wrong method

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      get '/sign_in', to: 'extension_auth#sign_in'
+      post '/sign_in', to: 'extension_auth#sign_in'
     end
   end
 end
