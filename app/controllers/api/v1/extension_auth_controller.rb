048aa448906192a38cd010bdd0025cd2c321aa49
yintii <keleheart1@gmail.com>
2024-02-02 09:22:46 -0800

    puts calls to debug

--- app/controllers/api/v1/extension_auth_controller.rb
+++ app/controllers/api/v1/extension_auth_controller.rb
@@ -1,9 +1,15 @@
 class Api::V1::ExtensionAuthController < ApplicationController
   def sign_in 
     user = User.find_by(email: params[:email])
+    puts 'looking for user with email: ' + params[:email]
+    puts 'user found: ' + user.to_s
+    puts 'user provided password: ' + params[:password]
+    puts 'checking to authenticate user'
     if user && user.authenticate(params[:password])
+      puts 'user authenticated'
       render json: { token: user.auth_token }
     else
+      puts 'user not authenticated'
       render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
   end
