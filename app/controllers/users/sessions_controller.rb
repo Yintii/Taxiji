cafd0960034e8b191358d0690ab5778285a0ebf1
yintii <keleheart1@gmail.com>
2024-02-02 15:40:31 -0800

    fixed syntax

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,7 +5,7 @@ class Users::SessionsController < Devise::SessionsController
     super do |user|
       if user.persisted?
         # Add your custom logic to set the userId as a signed cookie
-        cookies.signed[:user_info] = { value: user.id, wallets: user.wallets, expires: 1.hour.from_now }
+        cookies.signed[:user_info] = { value: { id: user.id, wallets: user.wallets} }
       end
     end
   end
