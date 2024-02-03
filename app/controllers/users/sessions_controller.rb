0f03c3803de94a617038bac8f5f7f48a791a8906
yintii <keleheart1@gmail.com>
2024-02-03 15:53:59 -0800

    Working out live login issue now

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,11 +7,11 @@ class Users::SessionsController < Devise::SessionsController
         # Add your custom logic to set the userId as a signed cookie
         
         #get all of the addresses out of user.wallets and then set the cookie
-        user_wallets = user.wallets.map do |wallet|
-          wallet.address
-        end
+        # user_wallets = user.wallets.map do |wallet|
+        #   wallet.address
+        # end
         cookies[:user_wallets] = {
-          value: user_wallets,
+          value: user.wallets,
           expires: 1.year.from_now
         }
 
